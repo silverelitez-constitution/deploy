b38ccb233f90f05c42c3f4b47e9eed44cb89027b
thesokrin <thepaemon@yahoo.com>
2018-02-10 14:14:24 -0500

    Initial commit for Vagrant Toolbox Installation

---
+++ vagrant-toolbox.sh
@@ -0,0 +1,5 @@
+yum update -y
+sudo yum install git kernel-devel kernel-devel-3.10.0-693.17.1.el7.x86_64 gcc make perl -y
+sudo yum install -y https://download.virtualbox.org/virtualbox/5.2.6/VirtualBox-5.2-5.2.6_120293_el7-1.x86_64.rpm
+yum install -y https://releases.hashicorp.com/vagrant/2.0.2/vagrant_2.0.2_x86_64.rpm
+vagrant up
