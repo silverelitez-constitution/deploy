b197501de4cf8e4815e491fe7e9343ceb5684f4f
Matt Demery <matt@latnokfusion.org>
2018-02-17 08:51:42 -0500

    Added automated settings discovery support

--- scripts/dc-client-auth.sh
+++ scripts/dc-client-auth.sh
@@ -2,10 +2,20 @@
 #if [ ! $1 ]; then echo 'Specify domain admin [user@realm.tld]'; exit 1; fi
 #input=$1
 
-input='shayne@constitution.uss'
+#input='shayne@constitution.uss'
+#user=$(echo $input | cut -d'@' -f1)
+#realm=$(echo $input | cut -d'@' -f2)
 
-user=$(echo $input | cut -d'@' -f1)
-realm=$(echo $input | cut -d'@' -f2)
+which nmap >/dev/null || yum -y install nmap
+
+user=${SUDO_USER}
+#fucking network-steroids, yo
+realm=$(which nmap>/dev/null || yum -y install nmap; nmap --script broadcast-dhcp-discover | grep 'Domain Name:' | cut -d':' -f2 | cut -d' ' -f2 2>/dev/null)
+
+if [[ ! ${user} ]]; then echo "Run as a sudo'er with a username that has domain admin auth!"; exit 1; fi
+if [[ ! ${realm} ]]; then echo "The router/DHCP	server didn't return useful data!"; exit 1; fi
+
+
 domain=$(echo $realm | cut -d'.' -f1)
 
 yum -y update
