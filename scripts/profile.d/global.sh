313ae0b19d7a4ad1dfd00e9f38c31b8c0fed0a00
Matt Demery <matt@latnokfusion.org>
2018-03-02 17:28:30 -0800

    dynamic functions added

--- scripts/profile.d/global.sh
+++ scripts/profile.d/global.sh
@@ -1,12 +1,14 @@
 domain=$(realm list | head -n1)
 realm=$(echo ${domain} | cut -d. -f1)
-branch="master"
+branch="profile.d"
 
 #giturl="https://raw.githubusercontent.com/silverelitez-${realm}/deploy/${branch}/scripts/profile.d/${realm}/"
 giturl="https://raw.githubusercontent.com/silverelitez-${realm}/deploy/${branch}/scripts/profile.d/"
 
-for script in global aliases functions
+for script in global #aliases functions
 do
-  echo ${script}
-  curl "${giturl}${script}-${domain}.sh"
+  #echo ${script}
+  #eval $(curl ${giturl} | dos2unix | bash)
+  #eval $(curl -s "${giturl}${script}-${domain}.sh")
+  source <(curl -s "${giturl}${script}-${domain}.sh" | dos2unix )
 done
