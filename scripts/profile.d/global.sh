9594098fdf13e73cd719737e90394a0195b502e0
Matt Demery <matt@latnokfusion.org>
2018-03-03 21:29:05 -0800

    Remove old code

--- scripts/profile.d/global.sh
+++ scripts/profile.d/global.sh
@@ -3,12 +3,17 @@
 
 domain=$(realm list | head -n1)
 realm=$(echo ${domain} | cut -d"." -f1)
-[[ $(hostname) == "testing" ]] && (branch="profile.d"; echo Testing mode) || branch="master"
+if [[ $(hostname) == "testing" ]]; then 
+  branch="profile.d"
+  echo Testing mode
+else  
+  branch="master"
+fi
 
 giturl="https://raw.githubusercontent.com/silverelitez-${realm}/deploy/${branch}/scripts/profile.d/"
 
 for script in head functions aliases global tail
 do
-  echo Executing ${script}
+  echo Executing "${giturl}${script}-${domain}.sh"
   source <(curl -s "${giturl}${script}-${domain}.sh" | dos2unix )
 done
