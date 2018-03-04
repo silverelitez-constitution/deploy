2122fb48ac100b217c9e454c8731914327a5cd73
Matt Demery <matt@latnokfusion.org>
2018-03-03 20:40:14 -0800

    touched up users group permissions

--- scripts/profile.d/tail-constitution.uss.sh
+++ scripts/profile.d/tail-constitution.uss.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
-export EDITOR='nano'
-
-# [shayne@vpc ~]$ fortune | cowsay
-# The package cowsay is required to run 'cowsay'! Installing...
-# Okay, now let's try that again...shall we?
-# [shayne@vpc ~]$ cowsay
- # ________________________________________
-# / Well, that's more-or-less what I was   \
-# | saying, though obviously addition is a |
-# | little more cosmic than the bitwise    |
-# | operators.                             |
-# |                                        |
-# | -- Larry Wall in                       |
-# \ <199709051808.LAA01780@wall.org>       /
- # ----------------------------------------
-        # \   ^__^
-         # \  (oo)\_______
-            # (__)\       )\/\
-                # ||----w |
-                # ||     ||
-# [shayne@vpc ~]$
+export EDITOR='/usr/bin/nano'
 
 GIT_PROMPT_ONLY_IN_REPO=1;
 source ~/.bash-git-prompt/gitprompt.sh
@@ -38,4 +18,8 @@ cal
 fortune
 date
 
-echo -n "You are a member of ";id $(whoami) | sed 's/,/\n/g' | grep -o '(domain admins)' && num_updates=$(yum list updates | grep epel | grep -v '* epel:' | cut -d' ' -f1 | wc -l) && echo ${num_updates} updates available.
+groups=$(id $(whoami) | sed 's/,/\n/g' | grep -oe "(.*)")
+echo "You are a member of: ";
+echo "${groups}"
+
+echo "${groups}" | grep -o '(domain admins)' >/dev/null && num_updates=$(yum list updates | grep epel | grep -v '* epel:' | cut -d' ' -f1 | wc -l) && echo ${num_updates} updates available.
