82dc1f706c88a210d900506f509a53e73b5bd923
Matt Demery <matthewtdemery@gmail.com>
2018-03-18 20:06:17 -0700

    add go v1.10 support

--- packages/vagrant.sh
+++ packages/vagrant.sh
@@ -10,8 +10,12 @@ else
   exit
 fi
 
+export PATH="${PATH}:/usr/local/go/bin"
+
 yum update -y
-sudo yum install git kernel-devel kernel-devel-3.10.0-693.17.1.el7.x86_64 gcc make perl -y
+sudo yum install jq awscli git kernel-devel kernel-devel-3.10.0-693.17.1.el7.x86_64 gcc make perl -y
 sudo yum install -y https://download.virtualbox.org/virtualbox/5.2.6/VirtualBox-5.2-5.2.6_120293_el7-1.x86_64.rpm
 yum install -y https://releases.hashicorp.com/vagrant/2.0.2/vagrant_2.0.2_x86_64.rpm
+curl https://dl.google.com/go/go1.10.linux-amd64.tar.gz | tar -C /usr/local -zx
+go get github.com/ccll/terraform-provider-virtualbox
 #vagrant up
