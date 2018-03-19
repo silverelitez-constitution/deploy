d9a23a16a101f75ab55aadf3c04d31161b55ca60
Matt Demery <matthewtdemery@gmail.com>
2018-03-18 20:37:59 -0700

    initial terraform image

--- resources/provisioner-constitution.uss.sh
+++ resources/provisioner-constitution.uss.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # This deployment script has NOT been ravenously tested on:
 # Centos 7, Ubuntu 17.10, Gentoo 17.1
+source /etc/os-release
+
 
 # Resources
 gitsource() {
@@ -38,10 +40,11 @@ q_install() {
 # Steps
 prepare_host() {
   echo "Preparing host..."
-  source /etc/os-release
   password=${1}
   q_install dos2unix
-  [ ${ID} == 'gentoo'] && { echo -e 'y\n' | layman -a sabayon; emerge realmd --quiet; } || { q_install realm realmd; q_install kinit krb5-user; }
+  q_install deltarpm applydeldarpm
+  ${P_INSTALL} nspr yum-utils
+  [ ${ID} == 'gentoo' ] && { echo -e 'y\n' | layman -a sabayon; emerge realmd --quiet; } || { q_install realm realmd; q_install kinit krb5-user; }
   echo Hostname: $(hostname | cut -d'.' -f1)
   if [[ "$(hostname | cut -d'.' -f1)" == "dc" ]]; then echo "Refusing to turn a domain controller into a client. Aborting..."; exit; fi
   [ ! ${realm} ] && { echo -n Discovering realm...;realm=$(sudo realm discover | head -n1);echo $realm; }
@@ -63,7 +66,7 @@ prepare_host() {
 }
 install_packages() {
   echo "Update packager..."
-  [[ ${ID} == 'centos' ]] && { echo "Install epel-release..."; ${P_INSTALL} epel-release; }
+  [[ ${ID} == 'centos' ]] && { echo "Install epel-release..."; ${P_INSTALL} deltarpm; ${P_INSTALL} epel-release; }
   [[ ${ID} == 'centos' ]] && { echo "Remove PackageKit-command-not-found..."; ${P_REMOVE} -y PackageKit-command-not-found; }
   P_UPDATE
   [[ ${ID} == 'centos' ]] && { yum-complete-transaction --cleanup-only; yum makecache --quiet; }
