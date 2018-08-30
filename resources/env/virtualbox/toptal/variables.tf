c4964db349fb89437ed521638d9519b5ca1e9989
Matt Demery <matthewtdemery@gmail.com>
2018-08-30 01:58:10 -0700

    general state file updates. will migrate to s3fs in next release

---
+++ resources/env/virtualbox/toptal/variables.tf
@@ -0,0 +1,53 @@
+# Flight Staging Account - IAM Resources - Variables
+
+variable "name" {
+  description = <<-EOF
+    Resource name.
+  EOF
+}
+
+variable "memory" {
+  description = <<-EOF
+    Amount of RAM.
+  EOF
+}
+
+variable "service" {
+  description = <<-EOF
+    Service to be hosted.
+  EOF
+}
+variable "url" {
+  description = <<-EOF
+    URL of VM Image.
+  EOF
+}
+variable "image" {
+  description = <<-EOF
+    Image for VM.
+  EOF
+}
+
+variable "imagedir" {
+  description = <<-EOF
+    Directory for image files.
+  EOF
+}
+
+variable "interface" {
+  description = <<-EOF
+    Interface to bridge to.
+  EOF
+}
+
+variable "cpus" {
+  description = <<-EOF
+    Amount of CPU's to set for VM.
+  EOF
+}
+
+variable "count" {
+  description = <<-EOF
+    Number of instances of this resource to launch.
+  EOF
+}
