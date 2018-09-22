2f8f179129925bc81f4bb830831380286954df38
Matt Demery <matthewtdemery@gmail.com>
2018-09-22 02:36:45 -0700

    Fixed bug bug

--- resources/translators/default.sh
+++ resources/translators/default.sh
@@ -8,7 +8,7 @@
 echo "You're using ${PRETTY_NAME}"
 echo "OS ID: ${ID}"
 echo "Visit us at ${HOME_URL}"
-echo "Got a distro related problem? Let us know at ${BUG_REPORT_URL}"
+[ ${BUG_REPORT_URL} ] && echo "Got a distro related problem? Let us know at ${BUG_REPORT_URL}"
 
 gitsource resources/translators/${ID}.sh
 [ ${_SHELL_TRANSLATOR} ] || { echo "Translator load error"; return 1; }
