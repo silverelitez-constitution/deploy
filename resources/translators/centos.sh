c1bcf7f68502ad23a6a454cd590d7c6b6c1d46f7
Matt Demery <matthewtdemery@gmail.com>
2018-09-21 23:31:29 -0700

    Removed the --quiet flag from update/upgrade commands

--- resources/translators/centos.sh
+++ resources/translators/centos.sh
@@ -5,8 +5,8 @@ P_REMOVE="sudo yum remove -t "
 P_BINARY() { repoquery -l ${1} | grep bin | rev | cut -d'/' -f1 | rev; }
 P_INSTALL_PIP() { sudo yum --quiet -y install gcc python34-pip python34-devel python34-setuptools; }
 P_UPDATES() { yum list updates | grep epel | grep -v '* epel:' | cut -d' ' -f1; }
-P_UPDATE() { sudo yum update -y --quiet; }
-P_UPGRADE() { sudo yum upgrade -y --quiet; }
+P_UPDATE() { sudo yum update -y; }
+P_UPGRADE() { sudo yum upgrade -y; }
 P_SEARCH() { yum search "${@}"; }
 PG_BASH_COMPLETION() {
   yum --quiet --cacheonly -y install *bash-complet*;
