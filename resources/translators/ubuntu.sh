3b87e282d43a7fbe3b097ea8c91121e6b41df3b0
Matt Demery <matthewtdemery@gmail.com>
2019-01-14 09:48:12 -0500

    adding ubuntu specific installer for kubernetes as it is not in package repo

--- resources/translators/ubuntu.sh
+++ resources/translators/ubuntu.sh
@@ -2,8 +2,8 @@ alias i='sudo yum -y install '
 P_INSTALL="apt -o Apt::Color=0 install -y "
 P_REMOVE="apt -o Apt::Color=0 remove -t "
 P_INSTALL_PIP() { sudo apt -o Apt::Color=0 --force-confnew,confdef --quiet -y install gcc python-pip; }
-P_UPDATE() { sudo apt -o Apt::Color=0 update -y --force-confnew,confdef; }
-P_UPGRADE() { sudo apt -o Apt::Color=0 upgrade -y --force-confnew,confdef; }
+P_UPDATE() { sudo apt -o Apt::Color=0 update -y --force-yes; }
+P_UPGRADE() { sudo apt -o Apt::Color=0 upgrade -y --force-yes; }
 P_NAME() { release="$(lsb_release -c | cut -f2)"; curl -s "https://packages.ubuntu.com/search?searchon=contents&keywords=${1}&mode=exactfilename&suite=${release}&arch=any" | grep "<a href=\"/${release}/" | cut -f3 -d'/' | cut -d'"' -f1 | head -n1; }
 P_SEARCH() { apt -o Apt::Color=0 search "${@}"; }
 P_BINARY() { apt-file list ${1} | grep -e "/games/${1}\|/bin/${1}"; }
