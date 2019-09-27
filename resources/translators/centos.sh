8a20399082241aec1f358f8973819bd0cc9cb402
Matt Demery <matthewtdemery@gmail.com>
2019-09-26 20:51:45 -0400

    fix typo in centos translator script

--- resources/translators/centos.sh
+++ resources/translators/centos.sh
@@ -1,7 +1,7 @@
 alias i='sudo yum -y install '
 P_INSTALL="sudo yum -t install -y "
 P_NAME() { repoquery --cache  -C --qf %{NAME} --whatprovides *bin/${1}; }
-P_REMOVE="sudo yum remove -t "
+P_REMOVE="sudo yum remove -y "
 P_BINARY() { repoquery -l ${1} | grep bin | rev | cut -d'/' -f1 | rev; }
 P_INSTALL_PIP() { sudo yum --quiet -y install gcc python34-pip python34-devel python34-setuptools; }
 P_UPDATES() { yum list updates | grep epel | grep -v '* epel:' | cut -d' ' -f1; }
