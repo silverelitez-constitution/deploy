a35c96e71e2c74df89c8f2ba3a56a035004cd911
Matt Demery <matthewtdemery@gmail.com>
2019-10-05 15:52:08 -0400

    try to update amzl software despite broken packages

--- resources/translators/amzn.sh
+++ resources/translators/amzn.sh
@@ -5,8 +5,8 @@ P_REMOVE="sudo yum remove -t "
 P_BINARY() { repoquery --plugins -l ${1} | grep bin | rev | cut -d'/' -f1 | rev; }
 P_INSTALL_PIP() { sudo yum --quiet -y install gcc python34-pip python34-devel python34-setuptools; }
 P_UPDATES() { yum list updates | grep epel | grep -v '* epel:' | cut -d' ' -f1; }
-P_UPDATE() { sudo yum update -y; sudo yum makecache; }
-P_UPGRADE() { sudo yum upgrade -y; sudo yum makecache; }
+P_UPDATE() { sudo yum update -y --skip-broken; sudo yum makecache; }
+P_UPGRADE() { sudo yum upgrade -y --skip-broken; sudo yum makecache; }
 P_SEARCH() { yum search "${@}"; }
 PG_BASH_COMPLETION() {
   yum --quiet --cacheonly -y install *bash-complet*;
