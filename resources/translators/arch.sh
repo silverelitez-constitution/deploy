eb0acfdd1af82917c5269ef80ecba68fd00b4a00
Matt Demery <matthewtdemery@gmail.com>
2020-01-20 16:18:26 -0500

    add arch translator stub

---
+++ resources/translators/arch.sh
@@ -0,0 +1,17 @@
+alias i='echo null'
+P_INSTALL="echo null"
+P_NAME() {echo null;}
+P_REMOVE="echo null"
+P_BINARY() { echo null;}
+P_INSTALL_PIP() { echo null; }
+P_UPDATES() { echo null; }
+P_UPDATE() { echo null; }
+P_UPGRADE() { echo null; }
+P_SEARCH() { echo null; }
+PG_BASH_COMPLETION() {
+  echo null;
+}
+
+P_DEFAULTS=""
+
+_SHELL_TRANSLATOR=1
