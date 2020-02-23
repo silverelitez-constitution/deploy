abb63ffab0b21089c42edecc23cfb5e0dc1aaf8b
Matt Demery <matthewtdemery@gmail.com>
2020-02-23 06:06:25 -0500

    elevate pacman for updating

--- resources/translators/arch.sh
+++ resources/translators/arch.sh
@@ -5,8 +5,8 @@ P_SEARCH() { pacman -Ss "${@}"; }
 P_NAME() { pkgfile -b "${@}"; }
 P_BINARY() { pacman -Fl "${@}" | grep 'bin/' | cut -d' ' -f2 | rev | cut -d'/' -f1 | rev | sed '/^$/d'; }
 P_UPDATES() { pacman -Qu; }
-P_UPDATE() { pacman -Syu; }
-P_UPGRADE() { pacman -Syu; }
+P_UPDATE() { sudo pacman -Syu; }
+P_UPGRADE() { sudo pacman -Syu; }
 P_INSTALL_PIP() { pacman -S python-pip; }
 PG_BASH_COMPLETION() {
   pacman -S bash-completion;
