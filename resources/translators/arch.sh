0126525452acc84af78ef34889c3fb8e0c5d7922
Matt Demery <matthewtdemery@gmail.com>
2020-02-23 04:07:34 -0500

    arch translation file

--- resources/translators/arch.sh
+++ resources/translators/arch.sh
@@ -1,15 +1,15 @@
-alias i='echo null'
-P_INSTALL="echo null"
-P_NAME() { echo null;}
-P_REMOVE="echo null"
-P_BINARY() { echo null;}
-P_INSTALL_PIP() { echo null; }
-P_UPDATES() { echo null; }
-P_UPDATE() { echo null; }
-P_UPGRADE() { echo null; }
-P_SEARCH() { echo null; }
+alias i='sudo pacman -S '
+P_INSTALL="sudo pacman -S "
+P_REMOVE="sudo pacman -Rs "
+P_SEARCH() { pacman -Ss "$(@)"; }
+P_NAME() { pkgfile -b "$(@)"; }
+P_BINARY() { pacman -Fl "$(@)" | grep 'bin/' | cut -d' ' -f2 | rev | cut -d'/' -f1 | rev | sed '/^$/d'; }
+P_UPDATES() { pacman -Qu; }
+P_UPDATE() { pacman -Syu; }
+P_UPGRADE() { pacman -Syu; }
+P_INSTALL_PIP() { pacman -S python-pip; }
 PG_BASH_COMPLETION() {
-  echo null;
+  pacman -S bash-completion;
 }
 
 P_DEFAULTS=""
