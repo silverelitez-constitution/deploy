fbd532da8f9f9882882d6c81df42d721faea3821
Matt Demery <matthewtdemery@gmail.com>
2020-02-23 04:17:24 -0500

    fix typo in arch translation file

--- resources/translators/arch.sh
+++ resources/translators/arch.sh
@@ -1,9 +1,9 @@
 alias i='sudo pacman -S '
 P_INSTALL="sudo pacman -S "
 P_REMOVE="sudo pacman -Rs "
-P_SEARCH() { pacman -Ss "$(@)"; }
-P_NAME() { pkgfile -b "$(@)"; }
-P_BINARY() { pacman -Fl "$(@)" | grep 'bin/' | cut -d' ' -f2 | rev | cut -d'/' -f1 | rev | sed '/^$/d'; }
+P_SEARCH() { pacman -Ss "${@}"; }
+P_NAME() { pkgfile -b "${@}"; }
+P_BINARY() { pacman -Fl "${@}" | grep 'bin/' | cut -d' ' -f2 | rev | cut -d'/' -f1 | rev | sed '/^$/d'; }
 P_UPDATES() { pacman -Qu; }
 P_UPDATE() { pacman -Syu; }
 P_UPGRADE() { pacman -Syu; }
