43da5f0664fd6bc03188668968988ac9a5c52263
Matt Demery <matthewtdemery@gmail.com>
2020-02-23 04:09:02 -0500

    don't set local timezone

--- scripts/profile.d/head-constitution.uss.sh
+++ scripts/profile.d/head-constitution.uss.sh
@@ -1,3 +1,3 @@
 PATH+=":/usr/games/"
-sudo ln -sf /usr/share/zoneinfo/America/Detroit /etc/localtime
+#sudo ln -sf /usr/share/zoneinfo/America/Detroit /etc/localtime
 which ntpdate >/dev/null && sudo ntpdate time.windows.com > /dev/null &
